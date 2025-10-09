7d629e0889c184d6fde78c88b63aacd667d102d7
philogag <philogag@qq.com>
2025-10-09 13:53:15 +0800

    enable plasma-nm for network manager

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -19,7 +19,10 @@
     variant = "";
   };
 
+  # some plugins for KDE Plasma Desktop 
   environment.systemPackages = with pkgs; [
     kdePackages.networkmanager-qt
+    kdePackages.modemmanager-qt
+    kdePackages.plasma-nm
   ];
 }
